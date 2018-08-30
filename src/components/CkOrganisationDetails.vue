9f760c1a4b3067a0941cfe3e7e7ff582043419e2
Matt Inamdar <matt@ayup.agency>
2018-08-30 11:19:29 +0100

    refactored organisation details table

--- src/components/CkOrganisationDetails.vue
+++ src/components/CkOrganisationDetails.vue
@@ -1,32 +1,32 @@
 <template>
   <gov-table>
     <template slot="body">
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header" scope="row">Organisation name</th>
-        <td class="govuk-table__cell">{{ organisation.name }}</td>
-      </tr>
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header" scope="row">Description</th>
-        <td class="govuk-table__cell">{{ organisation.description }}</td>
-      </tr>
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header" scope="row">Website Address</th>
-        <td class="govuk-table__cell">{{ organisation.url }}</td>
-      </tr>
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header" scope="row">Phone number</th>
-        <td class="govuk-table__cell">{{ organisation.phone }}</td>
-      </tr>
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header" scope="row">Email</th>
-        <td class="govuk-table__cell">{{ organisation.email }}</td>
-      </tr>
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header align-top" scope="row">Logo</th>
-        <td class="govuk-table__cell">
+      <gov-table-row>
+        <gov-table-header scope="row">Organisation name</gov-table-header>
+        <gov-table-cell>{{ organisation.name }}</gov-table-cell>
+      </gov-table-row>
+      <gov-table-row>
+        <gov-table-header scope="row">Description</gov-table-header>
+        <gov-table-cell>{{ organisation.description }}</gov-table-cell>
+      </gov-table-row>
+      <gov-table-row>
+        <gov-table-header scope="row">Website Address</gov-table-header>
+        <gov-table-cell>{{ organisation.url }}</gov-table-cell>
+      </gov-table-row>
+      <gov-table-row>
+        <gov-table-header scope="row">Phone number</gov-table-header>
+        <gov-table-cell>{{ organisation.phone }}</gov-table-cell>
+      </gov-table-row>
+      <gov-table-row>
+        <gov-table-header scope="row">Email</gov-table-header>
+        <gov-table-cell>{{ organisation.email }}</gov-table-cell>
+      </gov-table-row>
+      <gov-table-row>
+        <gov-table-header class="align-top" scope="row">Logo</gov-table-header>
+        <gov-table-cell>
           <img :src="apiUrl(`/organisations/${organisation.id}/logo`)" alt="Organisation logo">
-        </td>
-      </tr>
+        </gov-table-cell>
+      </gov-table-row>
     </template>
   </gov-table>
 </template>
