1ae719fd257c227e7552a1caec1ef78a7075462a
Matt Inamdar <matt@ayup.agency>
2018-08-30 12:42:38 +0100

    themed the editor

--- src/components/CkWysiwyg.vue
+++ src/components/CkWysiwyg.vue
@@ -1,18 +1,18 @@
 <template>
-  <wysiwyg
+  <medium-editor
     class="govuk-body ck-wysiwyg"
     :text="text"
-    :options="options"
+    :options="mergedOptions"
     @edit="onEdit"
   />
 </template>
 
 <script>
-import Wysiwyg from "vue2-medium-editor";
+import MediumEditor from "vue2-medium-editor";
 
 export default {
   name: "CkWysiwyg",
-  components: { Wysiwyg },
+  components: { MediumEditor },
   props: {
     text: {
       type: String,
@@ -27,17 +27,132 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      defaultOptions: {
+        toolbar: {
+          allowMultiParagraphSelection: true,
+          buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'],
+          diffLeft: 0,
+          diffTop: -10,
+          firstButtonClass: 'medium-editor-button-first',
+          lastButtonClass: 'medium-editor-button-last',
+          relativeContainer: null,
+          standardizeSelectionStart: false,
+          static: false
+        }
+      }
+    };
+  },
+  computed: {
+    mergedOptions() {
+      return {...this.defaultOptions, ...this.options};
+    }
+  },
   methods: {
     onEdit(payload) {
-      console.log(payload.api);
+      console.log(payload.api.getContent());
     }
   }
 };
 </script>
 
 <style lang="scss">
+@import "node_modules/medium-editor/src/sass/medium-editor";
+
 .ck-wysiwyg {
   padding: 5px;
   border: 2px solid #0b0c0c;
+}
+
+/* Taken from the flat theme */
+$medium-editor-bgcolor: #000;
+$medium-editor-border-color: #fff;
+$medium-editor-button-size: 60px;
+$medium-editor-button-active-text-color: #fff;
+$medium-editor-link-color: #fff;
+$medium-editor-placeholder-color: #fff;
+
+// theme rules
+.medium-toolbar-arrow-under:after {
+    top: $medium-editor-button-size;
+    border-color: $medium-editor-bgcolor transparent transparent transparent;
+}
+
+.medium-toolbar-arrow-over:before {
+    top: -8px;
+    border-color: transparent transparent $medium-editor-bgcolor transparent;
+}
+
+.medium-editor-toolbar {
+  background-color: $medium-editor-bgcolor;
+
+  li {
+    padding: 0;
+
+    button {
+      min-width: $medium-editor-button-size;
+      height: $medium-editor-button-size;
+      border: none;
+      border-right: 1px solid lighten($medium-editor-bgcolor, 20);
+      background-color: transparent;
+      color: $medium-editor-link-color;
+      transition: background-color .2s ease-in, color .2s ease-in;
+      &:hover {
+        background-color: darken($medium-editor-bgcolor, 20);
+        color: $medium-editor-button-active-text-color;
+      }
+    }
+
+    .medium-editor-button-active {
+      background-color: darken($medium-editor-bgcolor, 30);
+      color: $medium-editor-button-active-text-color;
+    }
+
+    .medium-editor-button-last {
+      border-right: none;
+    }
+  }
+}
+
+.medium-editor-toolbar-form {
+  .medium-editor-toolbar-input {
+    height: $medium-editor-button-size;
+    background: $medium-editor-bgcolor;
+    color: $medium-editor-link-color;
+
+    &::-webkit-input-placeholder {
+      color: $medium-editor-placeholder-color;
+      color: rgba($medium-editor-placeholder-color, .8);
+    }
+
+    &:-moz-placeholder { /* Firefox 18- */
+      color: $medium-editor-placeholder-color;
+      color: rgba($medium-editor-placeholder-color, .8);
+    }
+
+    &::-moz-placeholder {  /* Firefox 19+ */
+      color: $medium-editor-placeholder-color;
+      color: rgba($medium-editor-placeholder-color, .8);
+    }
+
+    &:-ms-input-placeholder {
+      color: $medium-editor-placeholder-color;
+      color: rgba($medium-editor-placeholder-color, .8);
+    }
+  }
+
+  a {
+    color: $medium-editor-link-color;
+  }
+}
+
+.medium-editor-toolbar-anchor-preview {
+  background: $medium-editor-bgcolor;
+  color: $medium-editor-link-color;
+}
+
+.medium-editor-placeholder:after {
+  color: lighten($medium-editor-bgcolor, 20);
 }
 </style>
