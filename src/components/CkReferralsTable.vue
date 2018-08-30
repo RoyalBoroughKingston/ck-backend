4e3c27b7d4b8fe2809ad90e65e5202dd5b5f5eb4
Matt Inamdar <matt@ayup.agency>
2018-08-30 11:26:55 +0100

    refactored referrals listing table

--- src/components/CkReferralsTable.vue
+++ src/components/CkReferralsTable.vue
@@ -1,29 +1,29 @@
 <template>
   <gov-table>
     <template slot="header">
-      <tr class="govuk-table__row">
-        <th class="govuk-table__header" scope="col">Reference no.</th>
-        <th class="govuk-table__header" scope="col">Service</th>
-        <th class="govuk-table__header" scope="col">Name</th>
-        <th class="govuk-table__header" scope="col">Status</th>
-        <th class="govuk-table__header" scope="col">Date/time</th>
-        <th class="govuk-table__header text-right" scope="col"></th>
-      </tr>
+      <gov-table-row>
+        <gov-table-header scope="col">Reference no.</gov-table-header>
+        <gov-table-header scope="col">Service</gov-table-header>
+        <gov-table-header scope="col">Name</gov-table-header>
+        <gov-table-header scope="col">Status</gov-table-header>
+        <gov-table-header scope="col">Date/time</gov-table-header>
+        <gov-table-header class="text-right" scope="col"></gov-table-header>
+      </gov-table-row>
     </template>
     <template slot="body">
-      <tr v-for="(referral, key) in referrals" :key="key" class="govuk-table__row">
-        <td class="govuk-table__cell" scope="row">{{ referral.reference }}</td>
-        <td class="govuk-table__cell">{{ referral.service.name }}</td>
-        <td class="govuk-table__cell">{{ referral.name }}</td>
-        <td class="govuk-table__cell">{{ humanReadableStatus(referral.status) }}</td>
-        <td class="govuk-table__cell">{{ formatDateTime(referral.created_at) }}</td>
-        <td class="govuk-table__cell text-right">
+      <gov-table-row v-for="referral in referrals" :key="referral.id">
+        <gov-table-cell>{{ referral.reference }}</gov-table-cell>
+        <gov-table-cell>{{ referral.service.name }}</gov-table-cell>
+        <gov-table-cell>{{ referral.name }}</gov-table-cell>
+        <gov-table-cell>{{ humanReadableStatus(referral.status) }}</gov-table-cell>
+        <gov-table-cell>{{ formatDateTime(referral.created_at) }}</gov-table-cell>
+        <gov-table-cell class="text-right">
           <gov-link :to="{ name: 'referrals-confirmation', params: { referral: referral.id } }">View</gov-link>
-        </td>
-      </tr>
-      <tr v-if="referrals.length === 0" class="govuk-table__row">
-        <td class="text-center" colspan="6">No referrals</td>
-      </tr>
+        </gov-table-cell>
+      </gov-table-row>
+      <gov-table-row v-if="referrals.length === 0">
+        <gov-table-cell class="text-center" colspan="6">No referrals</gov-table-cell>
+      </gov-table-row>
     </template>
   </gov-table>
 </template>
