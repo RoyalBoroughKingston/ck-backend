dcb97663f287f73dd2a3d92811124366496c0192
Matt Inamdar <matt@ayup.agency>
2018-08-30 10:06:24 +0100

    removed pages directory and moved all into views

--- src/router.js
+++ src/router.js
@@ -11,109 +11,109 @@ let router = new Router({
     {
       path: "/",
       name: "dashboard",
-      component: () => import("@/views/pages/Dashboard"),
+      component: () => import("@/views/Dashboard"),
       meta: { auth: true }
     },
     {
       path: "/login",
       name: "login",
-      component: () => import("@/views/pages/auth/Login"),
+      component: () => import("@/views/auth/Login"),
       meta: { guest: true }
     },
     {
       path: "/login/code",
       name: "login-code",
-      component: () => import("@/views/pages/auth/Code"),
+      component: () => import("@/views/auth/Code"),
       meta: { guest: true }
     },
     {
       path: "/forgotten-password",
       name: "forgotten-password",
-      component: () => import("@/views/pages/auth/ForgottenPassword"),
+      component: () => import("@/views/auth/ForgottenPassword"),
       meta: { guest: true }
     },
     {
       path: "/reset-password",
       name: "reset-password",
-      component: () => import("@/views/pages/auth/NewPassword"),
+      component: () => import("@/views/auth/NewPassword"),
       meta: { guest: true }
     },
     {
       path: "/logout",
       name: "logout",
-      component: () => import("@/views/pages/auth/Logout"),
+      component: () => import("@/views/auth/Logout"),
       meta: { auth: true }
     },
     {
       path: "/organisations",
       name: "organisations-index",
-      component: () => import("@/views/pages/organisations/Index"),
+      component: () => import("@/views/organisations/Index"),
       meta: { auth: true }
     },
     {
       path: "/organisations/create",
       name: "organisations-create",
-      component: () => import("@/views/pages/organisations/Create"),
+      component: () => import("@/views/organisations/Create"),
       meta: { auth: true }
     },
     {
       path: "/organisations/:organisation",
       name: "organisations-show",
-      component: () => import("@/views/pages/organisations/Show"),
+      component: () => import("@/views/organisations/Show"),
       meta: { auth: true }
     },
     {
       path: "/referrals",
       name: "referrals-index",
-      component: () => import("@/views/pages/referrals/Index"),
+      component: () => import("@/views/referrals/Index"),
       meta: { auth: true }
     },
     {
       path: "/referrals/:referral/confirmation",
       name: "referrals-confirmation",
-      component: () => import("@/views/pages/referrals/Confirmation"),
+      component: () => import("@/views/referrals/Confirmation"),
       meta: { auth: true }
     },
     {
       path: "/referrals/:referral",
       name: "referrals-show",
-      component: () => import("@/views/pages/referrals/Show"),
+      component: () => import("@/views/referrals/Show"),
       meta: { auth: true }
     },
     {
       path: "/locations",
       name: "locations-index",
-      component: () => import("@/views/pages/locations/Index"),
+      component: () => import("@/views/locations/Index"),
       meta: { auth: true }
     },
     {
       path: "/locations/create",
       name: "locations-create",
-      component: () => import("@/views/pages/locations/Create"),
+      component: () => import("@/views/locations/Create"),
       meta: { auth: true }
     },
     {
       path: "/locations/:location",
       name: "locations-show",
-      component: () => import("@/views/pages/locations/Show"),
+      component: () => import("@/views/locations/Show"),
       meta: { auth: true }
     },
     {
       path: "/users",
       name: "users-index",
-      component: () => import("@/views/pages/users/Index"),
+      component: () => import("@/views/users/Index"),
       meta: { auth: true }
     },
     {
       path: "/user/create",
       name: "users-create",
-      component: () => import("@/views/pages/users/Create"),
+      component: () => import("@/views/users/Create"),
       meta: { auth: true }
     },
     {
       path: "/user/:user",
       name: "users-show",
-      component: () => import("@/views/pages/users/Show"),
+      component: () => import("@/views/users/Show"),
       meta: { auth: true }
     }
   ]
