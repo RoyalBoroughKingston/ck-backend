9ce76cfa9dd609d1043ac196375961feb15b4f21
Matt Inamdar <matt@ayup.agency>
2018-08-30 21:10:43 +0100

    added who for tab

--- src/views/services/show/WhoForTab.vue
+++ src/views/services/show/WhoForTab.vue
@@ -4,8 +4,32 @@
     <gov-table>
       <template slot="body">
         <gov-table-row>
-          <gov-table-header scope="row" top>Heading</gov-table-header>
-          <gov-table-cell>Value</gov-table-cell>
+          <gov-table-header scope="row" top>Age of service user</gov-table-header>
+          <gov-table-cell>{{ service.criteria.age_group || "No specific requirement" }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
+          <gov-table-header scope="row" top>Disability requirements</gov-table-header>
+          <gov-table-cell>{{ service.criteria.disability || "No specific requirement" }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
+          <gov-table-header scope="row" top>Gender specific</gov-table-header>
+          <gov-table-cell>{{ service.criteria.gender || "No specific requirement" }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
+          <gov-table-header scope="row" top>Specific housing status</gov-table-header>
+          <gov-table-cell>{{ service.criteria.housing || "No specific requirement" }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
+          <gov-table-header scope="row" top>Employment</gov-table-header>
+          <gov-table-cell>{{ service.criteria.employment || "No specific requirement" }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
+          <gov-table-header scope="row" top>Language accessibility</gov-table-header>
+          <gov-table-cell>{{ service.criteria.language || "No specific requirement" }}</gov-table-cell>
+        </gov-table-row>
+        <gov-table-row>
+          <gov-table-header scope="row" top>Any other notes as to whom the service is aimed at/not appropriate for?</gov-table-header>
+          <gov-table-cell>{{ service.criteria.other || "No specific requirement" }}</gov-table-cell>
         </gov-table-row>
       </template>
     </gov-table>
