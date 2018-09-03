b8713096f35f179ad262474e4046e598091c8fef
Matt Inamdar <matt@ayup.agency>
2018-09-03 16:13:05 +0100

    refactored who for tab into separate component

--- src/views/services/Create.vue
+++ src/views/services/Create.vue
@@ -14,146 +14,66 @@
               :logo-form="logoForm"
               @clear="form.$errors.clear($event)"
               @clear-logo="logoForm.$errors.clear($event)"
+              @next="onNext"
               @update:name="form.name = $event"
               @update:url="form.url = $event"
               @update:logo="logoForm.file = $event"
               @update:intro="form.intro = $event"
               @update:description="form.description = $event"
-              @next="onNext"
             />
 
             <additional-info-tab
               v-if="tabs[1].active"
               :form="form"
               @clear="form.$errors.clear($event)"
+              @next="onNext"
               @update:wait_time="form.wait_time = $event"
               @update:is_free="form.is_free = $event"
               @update:fees_text="form.fees_text = $event"
               @update:fees_url="form.fees_url = $event"
               @update:testimonial="form.testimonial = $event"
               @update:video_embed="form.video_embed = $event"
-              @next="onNext"
             />
 
             <useful-info-tab
               v-if="tabs[2].active"
               :form="form"
               @clear="form.$errors.clear($event)"
-              @update:useful_infos_title="form.useful_infos[$event.index].title = $event.value"
-              @update:useful_infos_description="form.useful_infos[$event.index].description = $event.value"
-              @update:useful_infos_order="form.useful_infos[$event.index].order = $event.value"
               @add="form.useful_infos.push($event)"
               @delete="$delete(form.useful_infos, $event)"
               @next="onNext"
+              @update:useful_infos_title="form.useful_infos[$event.index].title = $event.value"
+              @update:useful_infos_description="form.useful_infos[$event.index].description = $event.value"
+              @update:useful_infos_order="form.useful_infos[$event.index].order = $event.value"
             />
 
             <contact-details-tab
               v-if="tabs[3].active"
               :form="form"
               @clear="form.$errors.clear($event)"
+              @add="form.social_medias.push($event)"
+              @delete="$delete(form.social_medias, $event)"
+              @next="onNext"
               @update:contact_name="form.contact_name = $event"
               @update:contact_phone="form.contact_phone = $event"
               @update:contact_email="form.contact_email = $event"
               @update:social_medias_type="form.social_medias[$event.index].type = $event.value"
               @update:social_medias_url="form.social_medias[$event.index].url = $event.value"
-              @add="form.social_medias.push($event)"
-              @delete="$delete(form.social_medias, $event)"
-              @next="onNext"
             />
 
-            <!-- Who is it for tab -->
-            <template v-if="tabs[4].active">
-              <gov-heading size="l">Who is it for?</gov-heading>
-              <gov-grid-row>
-                <gov-grid-column width="one-half">
-                  <gov-body>
-                    Use this section to add details as to who this service is for. If any of these
-                    criteria do not apply to your service, please leave them blank.
-                  </gov-body>
-                  <gov-section-break size="l" />
-
-                  <!-- Age group -->
-                  <criteria-input
-                    v-model="form.criteria.age_group"
-                    :form="form"
-                    path="criteria.age_group"
-                    @input="form.$errors.clear('criteria.age_group')"
-                    label="Age of service user (if applicable)"
-                    hint='e.g. "This service is for people 16+", or "This service is aimed at people nearig retirement"'
-                  />
-                  <!-- /Age group -->
-
-                  <!-- Disability -->
-                  <criteria-input
-                    v-model="form.criteria.disability"
-                    :form="form"
-                    path="criteria.disability"
-                    @input="form.$errors.clear('criteria.disability')"
-                    label="Disability Requirements / Restrictions (if applicable)"
-                    hint='e.g. "This service is for those with MS and their carers", or "For all people with disabilities and their carers"'
-                  />
-                  <!-- /Disability -->
-
-                  <!-- Gender -->
-                  <criteria-input
-                    v-model="form.criteria.gender"
-                    :form="form"
-                    path="criteria.gender"
-                    @input="form.$errors.clear('criteria.gender')"
-                    label="Gender Specific (if applicable)"
-                    hint='e.g. "Women only"'
-                  />
-                  <!-- /Gender -->
-
-                  <!-- Housing -->
-                  <criteria-input
-                    v-model="form.criteria.housing"
-                    :form="form"
-                    path="criteria.housing"
-                    @input="form.$errors.clear('criteria.housing')"
-                    label="Specific Housing status/needs (if applicable)"
-                    hint='e.g. "For people who are homeless or at risk of homelessness"'
-                  />
-                  <!-- /Housing -->
-
-                  <!-- Income -->
-                  <criteria-input
-                    v-model="form.criteria.income"
-                    :form="form"
-                    path="criteria.income"
-                    @input="form.$errors.clear('criteria.income')"
-                    label="Income level (if applicable)"
-                    hint='e.g. "Service is aimed at people claiming benefits or with Income support"'
-                  />
-                  <!-- /Income -->
-
-                  <!-- Language -->
-                  <criteria-input
-                    v-model="form.criteria.language"
-                    :form="form"
-                    path="criteria.language"
-                    @input="form.$errors.clear('criteria.language')"
-                    label="Language accessability (if applicable)"
-                    hint='e.g. "Instructors speak English, but open to all", or "This service is available in a number of languages - please contact me for more information"'
-                  />
-                  <!-- /Language -->
-
-                  <!-- Other -->
-                  <criteria-input
-                    v-model="form.criteria.other"
-                    :form="form"
-                    path="criteria.other"
-                    @input="form.$errors.clear('criteria.other')"
-                    label="Any other notes as to who the service is aimed at/not appropraite for?"
-                    hint='e.g. "This service is open to all", or "This service is aimed at people living in Chessington"'
-                  />
-                  <!-- /Other -->
-
-                  <gov-button @click="onNext" start>Next</gov-button>
-                </gov-grid-column>
-              </gov-grid-row>
-            </template>
-            <!-- /Who is it for tab -->
+            <who-for-tab
+              v-if="tabs[4].active"
+              :form="form"
+              @clear="form.$errors.clear($event)"
+              @update:age_group="form.criteria.age_group = $event"
+              @update:disability="form.criteria.disability = $event"
+              @update:employment="form.criteria.employment = $event"
+              @update:gender="form.criteria.gender = $event"
+              @update:housing="form.criteria.housing = $event"
+              @update:income="form.criteria.income = $event"
+              @update:language="form.criteria.language = $event"
+              @update:other="form.criteria.other = $event"
+            />
 
             <!-- Location tab -->
             <template v-if="tabs[5].active">
@@ -329,7 +249,7 @@ import DetailsTab from "@/views/services/create/DetailsTab";
 import AdditionalInfoTab from "@/views/services/create/AdditionalInfoTab";
 import UsefulInfoTab from "@/views/services/create/UsefulInfoTab";
 import ContactDetailsTab from "@/views/services/create/ContactDetailsTab";
-import CriteriaInput from "@/views/services/create/CriteriaInput";
+import WhoForTab from "@/views/services/create/WhoForTab";
 
 export default {
   name: "CreateService",
@@ -338,7 +258,7 @@ export default {
     AdditionalInfoTab,
     UsefulInfoTab,
     ContactDetailsTab,
-    CriteriaInput
+    WhoForTab
   },
   data() {
     return {
@@ -396,15 +316,7 @@ export default {
         { heading: "Who is it for?", active: false },
         { heading: "Locations", active: false },
         { heading: "Referral", active: false  }
-      ],
-      ageGroupEnabled: null,
-      disabilityEnabled: null,
-      employmentEnabled: null,
-      genderEnabled: null,
-      housingEnabled: null,
-      incomeEnabled: null,
-      languageEnabled: null,
-      otherEnabled: null
+      ]
     };
   },
   computed: {
@@ -431,46 +343,6 @@ export default {
     }
   },
   watch: {
-    ageGroupEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.age_group = "";
-      }
-    },
-    disabilityEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.disability = "";
-      }
-    },
-    employmentEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.employment = "";
-      }
-    },
-    genderEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.gender = "";
-      }
-    },
-    housingEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.housing = "";
-      }
-    },
-    incomeEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.income = "";
-      }
-    },
-    languageEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.language = "";
-      }
-    },
-    otherEnabled(enabled) {
-      if (!enabled) {
-        this.form.criteria.other = "";
-      }
-    },
     "form.referral_method"(newReferralMethod) {
       if ((newReferralMethod === null) || (newReferralMethod === 'none')) {
         this.form.referral_button_text = "";
---
+++ src/views/services/create/WhoForTab.vue
@@ -0,0 +1,162 @@
+<template>
+  <div>
+    <gov-heading size="l">Who is it for?</gov-heading>
+    <gov-grid-row>
+      <gov-grid-column width="one-half">
+        <gov-body>
+          Use this section to add details as to who this service is for. If any of these
+          criteria do not apply to your service, please leave them blank.
+        </gov-body>
+        <gov-section-break size="l" />
+
+        <!-- Age group -->
+        <criteria-input
+          :value="form.criteria.age_group"
+          @input="$emit('update:age_group', $event)"
+          :form="form"
+          path="criteria.age_group"
+          label="Age of service user (if applicable)"
+          hint='e.g. "This service is for people 16+", or "This service is aimed at people nearig retirement"'
+        />
+        <!-- /Age group -->
+
+        <!-- Disability -->
+        <criteria-input
+          :value="form.criteria.disability"
+          @input="$emit('update:disability', $event)"
+          :form="form"
+          path="criteria.disability"
+          label="Disability Requirements / Restrictions (if applicable)"
+          hint='e.g. "This service is for those with MS and their carers", or "For all people with disabilities and their carers"'
+        />
+        <!-- /Disability -->
+
+        <!-- Gender -->
+        <criteria-input
+          :value="form.criteria.gender"
+          @input="$emit('update:gender', $event)"
+          :form="form"
+          path="criteria.gender"
+          label="Gender Specific (if applicable)"
+          hint='e.g. "Women only"'
+        />
+        <!-- /Gender -->
+
+        <!-- Housing -->
+        <criteria-input
+          :value="form.criteria.housing"
+          @input="$emit('update:housing', $event)"
+          :form="form"
+          path="criteria.housing"
+          label="Specific Housing status/needs (if applicable)"
+          hint='e.g. "For people who are homeless or at risk of homelessness"'
+        />
+        <!-- /Housing -->
+
+        <!-- Income -->
+        <criteria-input
+          :value="form.criteria.income"
+          @input="$emit('update:income', $event)"
+          :form="form"
+          path="criteria.income"
+          label="Income level (if applicable)"
+          hint='e.g. "Service is aimed at people claiming benefits or with Income support"'
+        />
+        <!-- /Income -->
+
+        <!-- Language -->
+        <criteria-input
+          :value="form.criteria.language"
+          @input="$emit('update:language', $event)"
+          :form="form"
+          path="criteria.language"
+          label="Language accessability (if applicable)"
+          hint='e.g. "Instructors speak English, but open to all", or "This service is available in a number of languages - please contact me for more information"'
+        />
+        <!-- /Language -->
+
+        <!-- Other -->
+        <criteria-input
+          :value="form.criteria.other"
+          @input="$emit('update:other', $event)"
+          :form="form"
+          path="criteria.other"
+          label="Any other notes as to who the service is aimed at/not appropraite for?"
+          hint='e.g. "This service is open to all", or "This service is aimed at people living in Chessington"'
+        />
+        <!-- /Other -->
+
+        <gov-button @click="$emit('next')" start>Next</gov-button>
+      </gov-grid-column>
+    </gov-grid-row>
+  </div>
+</template>
+
+<script>
+import CriteriaInput from "@/views/services/create/CriteriaInput";
+
+export default {
+  name: "WhoForTab",
+  components: { CriteriaInput },
+  props: {
+    form: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      ageGroupEnabled: null,
+      disabilityEnabled: null,
+      employmentEnabled: null,
+      genderEnabled: null,
+      housingEnabled: null,
+      incomeEnabled: null,
+      languageEnabled: null,
+      otherEnabled: null
+    };
+  },
+  watch: {
+    ageGroupEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:age_group", "");
+      }
+    },
+    disabilityEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:disability", "");
+      }
+    },
+    employmentEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:employment", "");
+      }
+    },
+    genderEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:gender", "");
+      }
+    },
+    housingEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:housing", "");
+      }
+    },
+    incomeEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:income", "");
+      }
+    },
+    languageEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:language", "");
+      }
+    },
+    otherEnabled(enabled) {
+      if (!enabled) {
+        this.$emit("update:other", "");
+      }
+    }
+  }
+};
+</script>
