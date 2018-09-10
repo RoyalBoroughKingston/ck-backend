fced40b494231937fee7dc34dbdd17db1c0c804a
Matt Inamdar <matt@ayup.agency>
2018-09-10 09:31:54 +0100

    ignored .env files

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   pluginOptions: {
     s3Deploy: {
-      assetPath: 'dist',
-      bucket: 'admin.connectedkingston.uk',
-      region: 'eu-west-1',
+      assetPath: "dist",
+      bucket: "admin.connectedkingston.uk",
+      region: "eu-west-1",
       pwa: false,
       enableCloudfront: false,
       uploadConcurrency: 5
     }
   }
-}
+};
