24fc405020347f6d8649dc490a0c0d3c3f974569
Matt Inamdar <matt@ayup.agency>
2018-09-13 12:52:44 +0100

    removed staging test text

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,7 +7,7 @@
         <gov-grid-column width="two-thirds">
           <gov-heading size="xl">Dashboard</gov-heading>
           <gov-body size="l">
-            Hello and welcome to the Connected Kingston admin portal. [Staging]
+            Hello and welcome to the Connected Kingston admin portal.
           </gov-body>
         </gov-grid-column>
       </gov-grid-row>
