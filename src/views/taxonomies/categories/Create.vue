76cce5319c77c7e1895e002eccb98f290aef8991
Matt Inamdar <matt@ayup.agency>
2018-09-19 14:05:49 +0100

    edit screen working

--- src/views/taxonomies/categories/Create.vue
+++ src/views/taxonomies/categories/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <gov-width-container>
-    <vue-headful title="Connected Kingston - Taxonomies: Add Category" />
+    <vue-headful title="Connected Kingston - Add Taxonomy Category" />
 
     <gov-back-link :to="{ name: 'admin-index-taxonomies' }">Back to taxonomy categories</gov-back-link>
     <gov-main-wrapper>
