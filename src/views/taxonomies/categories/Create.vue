421230667174155b587da9bccf9baf65441cacf9
Matt Inamdar <matt@ayup.agency>
2018-09-19 13:23:40 +0100

    added create category page

--- src/views/taxonomies/categories/Create.vue
+++ src/views/taxonomies/categories/Create.vue
@@ -1,9 +1,59 @@
 <template>
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel aut sint corporis facilis, voluptatibus nisi blanditiis iusto eligendi laborum aliquam sed ipsa in vero a et, totam impedit cum veniam?</p>
+  <gov-width-container>
+    <vue-headful title="Connected Kingston - Taxonomies: Add Category" />
+
+    <gov-back-link :to="{ name: 'admin-index-taxonomies' }">Back to taxonomy categories</gov-back-link>
+    <gov-main-wrapper>
+      <gov-grid-row>
+        <gov-grid-column width="one-half">
+          <gov-heading size="xl">
+            <gov-caption size="xl">Taxonomies</gov-caption>
+            Categories
+          </gov-heading>
+          <gov-heading size="m">Add category</gov-heading>
+          <gov-body>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sed, labore accusantium culpa
+            praesentium illo assumenda eveniet dignissimos explicabo libero quibusdam ab a quia, vitae
+            odio. Laborum delectus quidem voluptatibus.
+          </gov-body>
+
+          <taxonomy-form
+            :errors="form.$errors"
+            :parent_id.sync="form.parent_id"
+            :name.sync="form.name"
+            :order.sync="form.order"
+            @clear="form.$errors.clear($event)"
+          />
+
+          <gov-button v-if="form.$submitting" disabled type="submit">Creating...</gov-button>
+          <gov-button v-else @click="onSubmit" type="submit">Create</gov-button>
+        </gov-grid-column>
+      </gov-grid-row>
+    </gov-main-wrapper>
+  </gov-width-container>
 </template>
 
 <script>
+import TaxonomyForm from "@/views/taxonomies/categories/forms/TaxonomyForm";
+import Form from "@/classes/Form";
+
 export default {
-  name: "CreateTaxonomyCategory"
+  name: "CreateTaxonomyCategory",
+  components: { TaxonomyForm },
+  data() {
+    return {
+      form: new Form({
+        parent_id: null,
+        name: "",
+        order: 1
+      })
+    };
+  },
+  methods: {
+    async onSubmit() {
+      await this.form.post("/taxonomies/categories");
+      this.$router.push({ name: "admin-index-taxonomies" });
+    }
+  }
 };
 </script>
--- src/views/taxonomies/categories/forms/TaxonomyForm.vue
+++ src/views/taxonomies/categories/forms/TaxonomyForm.vue
@@ -1,9 +1,86 @@
 <template>
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel aut sint corporis facilis, voluptatibus nisi blanditiis iusto eligendi laborum aliquam sed ipsa in vero a et, totam impedit cum veniam?</p>
+  <div>
+
+    <ck-text-input
+      :value="name"
+      @input="onInput('name', $event)"
+      id="name"
+      label="Name"
+      type="text"
+      :error="errors.get('name')"
+    />
+
+    <ck-loader v-if="loading" />
+    <template v-else>
+      <ck-select-input
+        :value="parent_id"
+        @input="onInput('parent_id', $event)"
+        id="parent_id"
+        label="Parent"
+        :options="taxonomyOptions"
+        :error="errors.get('parent_id')"
+      />
+    </template>
+
+  </div>
 </template>
 
 <script>
+import http from "@/http";
+
 export default {
-  name: "ListTaxonomyCategories"
+  name: "TaxonomyForm",
+  props: {
+    errors: {
+      required: true,
+      type: Object
+    },
+    parent_id: {
+      required: true
+    },
+    name: {
+      required: true
+    },
+    order: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      taxonomies: [],
+      taxonomyOptions: []
+    };
+  },
+  methods: {
+    onInput(field, value) {
+      this.$emit(`update:${field}`, value);
+      this.$emit("clear", field);
+    },
+    async fetchTaxonomies() {
+      this.loading = true;
+
+      const { data } = await http.get("/taxonomies/categories");
+      this.taxonomies = data.data;
+      this.taxonomyOptions = [
+        { text: "Please select...", value: null, disabled: true },
+        ...this.parseTaxonomies(this.taxonomies)
+      ];
+
+      this.loading = false;
+    },
+    parseTaxonomies(taxonomies) {
+      return taxonomies.map(taxonomy => {
+        let parsed = { text: taxonomy.name, value: taxonomy.id };
+        if (taxonomy.children.length > 0) {
+          parsed.options = this.parseTaxonomies(taxonomy.children);
+        }
+        return parsed;
+      });
+    }
+  },
+  created() {
+    this.fetchTaxonomies();
+  }
 };
 </script>
