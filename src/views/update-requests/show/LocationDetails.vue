d73ddf682c036f8412d34faf78b1f09f2a094cec
Matt Inamdar <matt@ayup.agency>
2019-01-21 13:16:18 +0000

    Made organisation fields optional

--- src/views/update-requests/show/LocationDetails.vue
+++ src/views/update-requests/show/LocationDetails.vue
@@ -2,52 +2,52 @@
   <gov-table>
     <template slot="body">
 
-      <gov-table-row v-if="location.address_line_1">
+      <gov-table-row v-if="location.hasOwnProperty('address_line_1')">
         <gov-table-header top scope="row">Address line 1</gov-table-header>
         <gov-table-cell>{{ location.address_line_1 }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.address_line_2">
+      <gov-table-row v-if="location.hasOwnProperty('address_line_2')">
         <gov-table-header top scope="row">Address line 2</gov-table-header>
         <gov-table-cell>{{ location.address_line_2 }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.address_line_3">
+      <gov-table-row v-if="location.hasOwnProperty('address_line_3')">
         <gov-table-header top scope="row">Address line 3</gov-table-header>
         <gov-table-cell>{{ location.address_line_3 }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.city">
+      <gov-table-row v-if="location.hasOwnProperty('city')">
         <gov-table-header top scope="row">City</gov-table-header>
         <gov-table-cell>{{ location.city }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.county">
+      <gov-table-row v-if="location.hasOwnProperty('county')">
         <gov-table-header top scope="row">County</gov-table-header>
         <gov-table-cell>{{ location.county }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.postcode">
+      <gov-table-row v-if="location.hasOwnProperty('postcode')">
         <gov-table-header top scope="row">Postcode</gov-table-header>
         <gov-table-cell>{{ location.postcode }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.country">
+      <gov-table-row v-if="location.hasOwnProperty('country')">
         <gov-table-header top scope="row">Country</gov-table-header>
         <gov-table-cell>{{ location.country }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.accessibility_info">
+      <gov-table-row v-if="location.hasOwnProperty('accessibility_info')">
         <gov-table-header top scope="row">Accessibility info</gov-table-header>
         <gov-table-cell>{{ location.accessibility_info }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.has_induction_loop">
+      <gov-table-row v-if="location.hasOwnProperty('has_induction_loop')">
         <gov-table-header top scope="row">Has induction loop</gov-table-header>
         <gov-table-cell>{{ location.has_induction_loop ? "Yes" : "No" }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row v-if="location.has_wheelchair_access">
+      <gov-table-row v-if="location.hasOwnProperty('has_wheelchair_access')">
         <gov-table-header top scope="row">Has wheelchair access</gov-table-header>
         <gov-table-cell>{{ location.has_wheelchair_access ? "Yes" : "No" }}</gov-table-cell>
       </gov-table-row>
--- src/views/update-requests/show/OrganisationDetails.vue
+++ src/views/update-requests/show/OrganisationDetails.vue
@@ -2,32 +2,32 @@
   <gov-table>
     <template slot="body">
 
-      <gov-table-row>
+      <gov-table-row v-if="organisation.hasOwnProperty('url')">
         <gov-table-header top scope="row">URL</gov-table-header>
         <gov-table-cell break>{{ organisation.url }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row>
+      <gov-table-row v-if="organisation.hasOwnProperty('name')">
         <gov-table-header top scope="row">Name</gov-table-header>
         <gov-table-cell>{{ organisation.name }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row>
+      <gov-table-row v-if="organisation.hasOwnProperty('slug')">
         <gov-table-header top scope="row">Slug</gov-table-header>
         <gov-table-cell>{{ organisation.slug }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row>
+      <gov-table-row v-if="organisation.hasOwnProperty('email')">
         <gov-table-header top scope="row">Email</gov-table-header>
         <gov-table-cell>{{ organisation.email }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row>
+      <gov-table-row v-if="organisation.hasOwnProperty('phone')">
         <gov-table-header top scope="row">Phone</gov-table-header>
         <gov-table-cell>{{ organisation.phone }}</gov-table-cell>
       </gov-table-row>
 
-      <gov-table-row>
+      <gov-table-row v-if="organisation.hasOwnProperty('description')">
         <gov-table-header top scope="row">Description</gov-table-header>
         <gov-table-cell v-html="toHtml(organisation.description)" />
       </gov-table-row>
