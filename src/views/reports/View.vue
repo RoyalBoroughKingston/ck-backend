5309d66a1319b35a910d060d76a5d3b5146116d6
Matt Inamdar <matt@ayup.agency>
2019-02-18 16:13:28 +0000

    Added view all reports tab

--- src/views/reports/View.vue
+++ src/views/reports/View.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
     <gov-heading size="l">View all reports</gov-heading>
+
+    <ck-resource-listing-table
+      ref="reportsTable"
+      uri="/reports"
+      :columns="[
+        { heading: 'Type', render: (report) => report.report_type },
+        { heading: 'From', render: (report) => report.starts_at ? formatDate(report.starts_at) : 'N/A' },
+        { heading: 'To', render: (report) => report.ends_at ? formatDate(report.ends_at) : 'N/A' },
+      ]"
+      :view-route="(report) => {
+        return {
+          name: 'reports-show',
+          params: { report: report.id }
+        }
+      }"
+    />
   </div>
 </template>
 
 <script>
+import CkResourceListingTable from "@/components/Ck/CkResourceListingTable.vue";
+
 export default {
   name: "ViewReportsPage",
+  components: {CkResourceListingTable},
 }
 </script>
