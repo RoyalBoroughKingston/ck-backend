507a972a222a0357f9311f8c0d28907675ba27da
Matt Inamdar <matt@ayup.agency>
2019-02-19 11:14:51 +0000

    Using POST request to fetch all services from new index route

--- src/main.js
+++ src/main.js
@@ -189,15 +189,19 @@ Vue.mixin({
     toHtml(markdown) {
       return MarkdownConverter.toHtml(markdown);
     },
-    async fetchAll(uri, params = {}) {
+    async fetchAll(uri, params = {}, method = "get") {
       let page = 1;
       let perPage = 100;
       let mergedParams = { ...params, page, per_page: perPage };
       let allFetched = false;
       let resources = [];
 
+      method = method.toLowerCase();
+      const payload =
+        method === "get" ? { params: mergedParams } : mergedParams;
+
       do {
-        const { data } = await http.get(uri, { params: mergedParams });
+        const { data } = await http[method](uri, payload);
         resources = [...resources, ...data.data];
         allFetched = data.meta.current_page === data.meta.last_page;
         mergedParams.page++;
