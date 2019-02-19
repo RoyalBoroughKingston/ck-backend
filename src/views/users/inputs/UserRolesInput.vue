1b02a49c415a36efa6c582d4b8f7d32ebf46e238
Matt Inamdar <matt@ayup.agency>
2019-02-19 15:14:13 +0000

    fixed style issues

--- src/views/users/inputs/UserRolesInput.vue
+++ src/views/users/inputs/UserRolesInput.vue
@@ -244,9 +244,11 @@ export default {
         };
       }
 
-      let services = await this.fetchAll("/services/index", {
-        "filter[organisation_id]": organisationIds.join(",")
-      }, "POST");
+      let services = await this.fetchAll(
+        "/services/index",
+        { "filter[organisation_id]": organisationIds.join(",") },
+        "POST"
+      );
 
       for (let organisationId of organisationIds) {
         this.services[organisationId].items = [
