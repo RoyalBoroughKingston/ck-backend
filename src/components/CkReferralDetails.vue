7e4a18adef3dd7867665da3eea6665fe88633309
Matthew Inamdar <matthew@inamdar.co.uk>
2019-02-22 11:36:48 +0000

    Updated labels and added new details (#89)

--- src/components/CkReferralDetails.vue
+++ src/components/CkReferralDetails.vue
@@ -5,58 +5,86 @@
         <gov-table-header top scope="row">Reference no.</gov-table-header>
         <gov-table-cell>{{ referral.reference }}</gov-table-cell>
       </gov-table-row>
+
       <gov-table-row>
         <gov-table-header top scope="row">Type</gov-table-header>
         <gov-table-cell>{{ type }}</gov-table-cell>
       </gov-table-row>
+
       <gov-table-row>
-        <gov-table-header top scope="row">Service</gov-table-header>
-        <gov-table-cell>{{ referral.service.name }}</gov-table-cell>
-      </gov-table-row>
-      <gov-table-row>
-        <gov-table-header top scope="row">Name</gov-table-header>
+        <gov-table-header top scope="row">Person Signposted</gov-table-header>
         <gov-table-cell>{{ referral.name }}</gov-table-cell>
       </gov-table-row>
+
       <gov-table-row>
-        <gov-table-header top scope="row">Email</gov-table-header>
+        <gov-table-header top scope="row">Signposted Email</gov-table-header>
         <gov-table-cell>{{ referral.email || "-" }}</gov-table-cell>
       </gov-table-row>
+
       <gov-table-row>
-        <gov-table-header top scope="row">Phone</gov-table-header>
+        <gov-table-header top scope="row">Signposted Phone</gov-table-header>
         <gov-table-cell>{{ referral.phone || "-" }}</gov-table-cell>
       </gov-table-row>
+
+      <gov-table-row>
+        <gov-table-header top scope="row">Signposted To</gov-table-header>
+        <gov-table-cell>{{ referral.service.name }}</gov-table-cell>
+      </gov-table-row>
+
+      <gov-table-row v-if="referral.service.contact_name">
+        <gov-table-header top scope="row">Signposted To Name</gov-table-header>
+        <gov-table-cell>{{ referral.service.contact_name }}</gov-table-cell>
+      </gov-table-row>
+
+      <gov-table-row v-if="referral.service.contact_email">
+        <gov-table-header top scope="row">Signposted To Email</gov-table-header>
+        <gov-table-cell>{{ referral.service.contact_email }}</gov-table-cell>
+      </gov-table-row>
+
+      <gov-table-row v-if="referral.service.contact_phone">
+        <gov-table-header top scope="row">Signposted To Phone</gov-table-header>
+        <gov-table-cell>{{ referral.service.contact_phone }}</gov-table-cell>
+      </gov-table-row>
+
       <template v-if="!isSelfReferral">
         <gov-table-row>
-          <gov-table-header top scope="row">Referee Name</gov-table-header>
+          <gov-table-header top scope="row">Champion Name</gov-table-header>
           <gov-table-cell>{{ referral.referee_name }}</gov-table-cell>
         </gov-table-row>
+
         <gov-table-row>
-          <gov-table-header top scope="row">Referee Email</gov-table-header>
+          <gov-table-header top scope="row">Champion Email</gov-table-header>
           <gov-table-cell>{{ referral.referee_email || "-" }}</gov-table-cell>
         </gov-table-row>
+
         <gov-table-row>
-          <gov-table-header top scope="row">Referee Phone</gov-table-header>
+          <gov-table-header top scope="row">Champion Phone</gov-table-header>
           <gov-table-cell>{{ referral.referee_phone || "-" }}</gov-table-cell>
         </gov-table-row>
+
         <gov-table-row>
-          <gov-table-header top scope="row">Referee Organisation</gov-table-header>
+          <gov-table-header top scope="row">Champion Organisation</gov-table-header>
           <gov-table-cell>{{ referral.referee_organisation }}</gov-table-cell>
         </gov-table-row>
       </template>
+
       <gov-table-row>
         <gov-table-header top scope="row">Consent</gov-table-header>
         <gov-table-cell>{{ consented }}</gov-table-cell>
       </gov-table-row>
+
       <gov-table-row>
         <gov-table-header top scope="row">Status</gov-table-header>
         <gov-table-cell>
           <gov-tag>{{ humanReadableStatus(referral.status) }}</gov-tag>
         </gov-table-cell>
       </gov-table-row>
+
       <gov-table-row>
         <gov-table-header top scope="row">Date/Time</gov-table-header>
         <gov-table-cell>{{ formatDateTime(referral.created_at) }}</gov-table-cell>
       </gov-table-row>
+
       <gov-table-row v-if="referral.status === 'completed'">
         <gov-table-header top scope="row">Scheduled for deletion</gov-table-header>
         <gov-table-cell>{{ formatDate(autoDeleteDate(referral.updated_at)) }}</gov-table-cell>
