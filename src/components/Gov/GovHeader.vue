2c9f84f4fa033c8d17a895ad2b0db872ddfd640e
Matt Inamdar <matt@ayup.agency>
2019-03-13 10:58:56 +0000

    Added nav item to user and relocated logout button

--- src/components/Gov/GovHeader.vue
+++ src/components/Gov/GovHeader.vue
@@ -9,9 +9,17 @@
                 </router-link>
             </div>
             <div class="govuk-header__content">
-                <router-link :to="{ name: 'dashboard' }" class="govuk-header__link govuk-header__link--service-name">
+                <div class="ck-header__main">
+                  <router-link :to="{ name: 'dashboard' }" class="govuk-header__link govuk-header__link--service-name">
                     {{ serviceName }}
-                </router-link>
+                  </router-link>
+
+                  <div v-if="auth.isLoggedIn && auth.user">
+                    <router-link :to="{ name: 'users-show', params: { user: auth.user.id } }" class="govuk-header__link">{{ auth.user.first_name }} {{ auth.user.last_name }}</router-link>
+                    &nbsp;
+                    <router-link :to="{ name: 'logout' }" class="govuk-header__link">Logout</router-link>
+                  </div>
+                </div>
                 <button @click="onToggleMenu" role="button" class="govuk-header__menu-button js-header-toggle" aria-controls="navigation" aria-label="Show or hide Top Level Navigation">Menu</button>
                 <nav>
                     <ul id="navigation" class="govuk-header__navigation" :class="{ 'govuk-header__navigation--open': navExpanded }" aria-label="Top Level Navigation">
@@ -71,3 +79,12 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.ck-header__main {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+</style>
