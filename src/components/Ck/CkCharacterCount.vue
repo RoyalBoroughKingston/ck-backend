9cbce4d653a58e3327f5779dd6c4168ec26b6061
Matt Inamdar <matt@ayup.agency>
2019-03-15 10:28:59 +0000

    Added character counter to textarea inputs

---
+++ src/components/Ck/CkCharacterCount.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ck-character-count">
+    {{ content.length }}/{{ maxLength }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CkCharacterCount',
+
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+
+    maxLength: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+.ck-character-count {
+  position: absolute;
+  bottom: -1.6rem;
+  right: 0;
+  color: #6f777b;
+}
+</style>
---
+++ src/components/Ck/CkCharacterCountGroup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ck-character-count-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CkCharacterCountGroup'
+}
+</script>
+
+<style lang="scss">
+.ck-character-count-group {
+  position: relative;
+}
+</style>
+
