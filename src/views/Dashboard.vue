89f2b7ebf4a0353689af02d22c4905dc5221aafa
Matthew Inamdar <matt@goldspecdigital.com>
2019-06-07 13:15:42 +0100

    Added links to dashboard buttons

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -25,55 +25,73 @@
         <gov-grid-column width="one-half">
           <gov-heading size="l">Services</gov-heading>
           <gov-body>Add or edit your pages on Connected Kingston.</gov-body>
-          <gov-button start>Go to services</gov-button>
+          <gov-button start :to="{ name: 'services-index' }">
+            Go to services
+          </gov-button>
           <gov-section-break size="m" />
         </gov-grid-column>
 
         <gov-grid-column width="one-half">
           <gov-heading size="l">Locations</gov-heading>
           <gov-body>View and edit service locations in the Borough.</gov-body>
-          <gov-button start>Go to locations</gov-button>
+          <gov-button start :to="{ name: 'locations-index' }">
+            Go to locations
+          </gov-button>
           <gov-section-break size="m" />
         </gov-grid-column>
 
         <gov-grid-column width="one-half">
           <gov-heading size="l">Referrals</gov-heading>
           <gov-body>View and respond to referrals to your service(s).</gov-body>
-          <gov-button start>Go to referrals</gov-button>
+          <gov-button start :to="{ name: 'referrals-index' }">
+            Go to referrals
+          </gov-button>
           <gov-section-break size="m" />
         </gov-grid-column>
 
         <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
           <gov-heading size="l">Organisations</gov-heading>
           <gov-body>Add or edit organisations on Connected Kingston.</gov-body>
-          <gov-button start>Go to organisations</gov-button>
+          <gov-button start :to="{ name: 'organisations-index' }">
+            Go to organisations
+          </gov-button>
           <gov-section-break size="m" />
         </gov-grid-column>
 
         <gov-grid-column width="one-half">
           <gov-heading size="l">Users</gov-heading>
           <gov-body>View, add and edit users in your organisation.</gov-body>
-          <gov-button start>Go to users</gov-button>
+          <gov-button start :to="{ name: 'users-index' }">
+            Go to users
+          </gov-button>
           <gov-section-break size="m" />
         </gov-grid-column>
 
         <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
           <gov-heading size="l">Reports</gov-heading>
-          <gov-body>Download reports of activity on Connected Kingston.</gov-body>
-          <gov-button start>Go to reports</gov-button>
+          <gov-body>
+            Download reports of activity on Connected Kingston.
+          </gov-body>
+          <gov-button start :to="{ name: 'reports-index' }">
+            Go to reports
+          </gov-button>
           <gov-section-break size="m" />
         </gov-grid-column>
 
         <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
           <gov-heading size="l">Admin</gov-heading>
           <gov-body>Admin tools for maintaining the site.</gov-body>
-          <gov-button start>Go to admin</gov-button>
+          <gov-button start :to="{ name: 'admin-index' }">
+            Go to admin
+          </gov-button>
         </gov-grid-column>
 
         <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
           <gov-heading size="l">Update Requests</gov-heading>
           <gov-body>View, approve and reject update requests.</gov-body>
-          <gov-button start>Go to update requests</gov-button>
+          <gov-button start :to="{ name: 'update-requests-index' }">
+            Go to update requests
+          </gov-button>
         </gov-grid-column>
       </gov-grid-row>
     </gov-main-wrapper>
