29801b351e9c8aa6d938a349494e185028160c34
Matthew Inamdar <matt@goldspecdigital.com>
2019-06-07 12:57:22 +0100

    Added new dashboard content

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,10 +5,75 @@
     <gov-main-wrapper>
       <gov-grid-row>
         <gov-grid-column width="two-thirds">
-          <gov-heading size="xl">Dashboard</gov-heading>
+          <gov-heading size="xl">
+            Welcome to the Connected Kingston admin portal
+          </gov-heading>
+
           <gov-body size="l">
-            Hello and welcome to the Connected Kingston admin portal.
+            From here, you can add and edit your pages on Connected Kingston, as
+            well as manage referrals into your service. For any support, contact
+            <gov-link href="mailto:info@connectedkingston.uk">
+              info@connectedkingston.uk
+            </gov-link>
           </gov-body>
+        </gov-grid-column>
+      </gov-grid-row>
+
+      <gov-section-break size="l" />
+
+      <gov-grid-row>
+        <gov-grid-column width="one-half">
+          <gov-heading size="l">Services</gov-heading>
+          <gov-body>Add or edit your pages on Connected Kingston.</gov-body>
+          <gov-button start>Go to services</gov-button>
+          <gov-section-break size="m" />
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half">
+          <gov-heading size="l">Locations</gov-heading>
+          <gov-body>View and edit service locations in the Borough.</gov-body>
+          <gov-button start>Go to locations</gov-button>
+          <gov-section-break size="m" />
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half">
+          <gov-heading size="l">Referrals</gov-heading>
+          <gov-body>View and respond to referrals to your service(s).</gov-body>
+          <gov-button start>Go to referrals</gov-button>
+          <gov-section-break size="m" />
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
+          <gov-heading size="l">Organisations</gov-heading>
+          <gov-body>Add or edit organisations on Connected Kingston.</gov-body>
+          <gov-button start>Go to organisations</gov-button>
+          <gov-section-break size="m" />
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half">
+          <gov-heading size="l">Users</gov-heading>
+          <gov-body>View, add and edit users in your organisation.</gov-body>
+          <gov-button start>Go to users</gov-button>
+          <gov-section-break size="m" />
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
+          <gov-heading size="l">Reports</gov-heading>
+          <gov-body>Download reports of activity on Connected Kingston.</gov-body>
+          <gov-button start>Go to reports</gov-button>
+          <gov-section-break size="m" />
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
+          <gov-heading size="l">Admin</gov-heading>
+          <gov-body>Admin tools for maintaining the site.</gov-body>
+          <gov-button start>Go to admin</gov-button>
+        </gov-grid-column>
+
+        <gov-grid-column width="one-half" v-if="auth.isGlobalAdmin">
+          <gov-heading size="l">Update Requests</gov-heading>
+          <gov-body>View, approve and reject update requests.</gov-body>
+          <gov-button start>Go to update requests</gov-button>
         </gov-grid-column>
       </gov-grid-row>
     </gov-main-wrapper>
@@ -16,7 +81,15 @@
 </template>
 
 <script>
+import Auth from "@/classes/Auth";
+
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+
+  data() {
+    return {
+      auth: Auth
+    };
+  }
 };
 </script>
