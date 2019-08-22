494ff7676de40adfa369819f4b55a0ede510660f
Matt Inamdar <matt@ayup.agency>
2019-08-22 13:36:27 +0100

    feature/move-to-gov-paas-for-production (#134)
    * Deploy script updated
    
    * Added CloudFormation template
    
    * Fixed CFN template
    
    * CI fix
    
    * Using npm ci instead

--- vue.config.js
+++ vue.config.js
@@ -8,10 +8,9 @@ module.exports = {
   pluginOptions: {
     s3Deploy: {
       assetPath: "dist",
-      bucket: "admin.connectedkingston.uk",
       region: "eu-west-1",
       pwa: false,
-      enableCloudfront: false,
+      enableCloudfront: true,
       cloudfrontMatchers: "/*",
       uploadConcurrency: 5
     }
